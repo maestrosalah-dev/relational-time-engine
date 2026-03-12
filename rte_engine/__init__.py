87283d741abbdb61b4737d7017c6a8a1507b5f0e
Athmani Salah <maestro.salah@gmail.com>
2026-03-12 04:13:44 +0100

    Update __init__.py

--- rte_engine/__init__.py
+++ rte_engine/__init__.py
@@ -2,3 +2,4 @@ from .policy import DriftExitPolicy
 from .wrapper import RTEGate
 from .distilbert_gate import DistilBERTRTEGate
 from .gpt2_gate import GPT2RTEGate
+from .vllm_adapter_sketch import VLLMAdapterSketch, RTEPolicyConfig, RTERuntimeState
