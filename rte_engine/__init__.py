8fe63916a365c87abef64d85409f14dd694b2874
Athmani Salah <maestro.salah@gmail.com>
2026-03-12 04:52:30 +0100

    Update __init__.py

--- rte_engine/__init__.py
+++ rte_engine/__init__.py
@@ -3,3 +3,4 @@ from .wrapper import RTEGate
 from .distilbert_gate import DistilBERTRTEGate
 from .gpt2_gate import GPT2RTEGate
 from .vllm_adapter_sketch import VLLMAdapterSketch, RTEPolicyConfig, RTERuntimeState
+from .mock_decoder_runtime import MockDecoderRuntime, DecoderStepTrace
